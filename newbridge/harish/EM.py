20ba9a9f0214e5f39be116075f0e3434b57bbf91
Shagun Rawat <shagunrawat27@gmail.com>
2018-03-04 16:13:11 -0800

    Corrected the 2 variable parallelization. Hardcoded the hermite polynomials so that drift function runs efficiently. Added comments.

--- newbridge/harish/EM.py
+++ newbridge/harish/EM.py
@@ -1,104 +1,45 @@
 import numpy as np
 import newbridge as nb
-from joblib import Parallel, delayed
 
 # load data
 import pickle
 with open('nem.pkl','rb') as f:
     allx, allt = pickle.load(f)
 
+# to check if x and t array are of correct shapes
 print(allx.shape)
 print(allt.shape)
 
 # initial guess for theta
-theta = np.array([1.5, 1., 0., -0.25])
+theta = np.array([2., 1., 0., -0.1])
+
+# degree of freedom is the highest degree polynomial in
+# the hermite series, which is the number of theta parameters
 dof = theta.shape[0]
 
 # true diffusion coefficient
 g = 0.5
 
-numsubintervals = 10
+# number of sub intervals in each interval [x_i, x_{i+1}] for
+# the Brownian bridge
+numsubintervals = 20
+
+# time step for EM
 h = (allt[1,0] - allt[0,0])/numsubintervals
+
+# number of Brownian bridge sample paths sampled
 numpaths = 1000
-burninpaths = 10
 
-def mcmc(burninpaths, numpaths, g, allx, allt, numsubintervals, i, j, h, theta, dof):
-    mmat = np.zeros((dof, dof))
-    rvec = np.zeros(dof)
-    x = allx[:,j]
-    t = allt[:,j]
-    
-    samples = np.zeros(numsubintervals)
-    _, xcur = nb.brownianbridge(g,x,t,numsubintervals,i)
-    oldlik = nb.girsanov(g=g, path=xcur, dt=h, theta=theta)
-    arburn = np.zeros(burninpaths)
-    for jj in range(burninpaths):
-        _, prop = nb.brownianbridge(g,x,t,numsubintervals,i)
-        proplik = nb.girsanov(g=g, path=prop, dt=h, theta=theta)
-        rho = np.exp(proplik - oldlik)
-        if (rho > np.random.uniform()):
-            xcur = prop
-            oldlik = proplik
-            arburn[jj] = 1
-    meanBurnin = np.mean(arburn)
-    
-    # for each path being sampled (r = 0 to r = R)
-    arsamp = np.zeros(numpaths)
-    for jj in range(numpaths):
-        _, prop = nb.brownianbridge(g,x,t,numsubintervals,i)
-        proplik = nb.girsanov(g=g, path=prop, dt=h, theta=theta)
-        rho = np.exp(proplik - oldlik)
-        if (rho > np.random.uniform()):
-            xcur = prop
-            oldlik = proplik
-            arsamp[jj] = 1
-        samples = xcur
-        pp = nb.mypoly(samples[:(-1)], dof)
-        mmat = mmat + h * np.matmul(pp.T, pp) / numpaths
-        rvec = rvec + np.matmul((np.diff(samples)).T, pp) / numpaths    
-    meanSample = np.mean(arsamp)
-    
-    return (mmat, rvec, meanBurnin, meanSample)
+# number of Brownian bridge 
+burninpaths = 100
 
-done = False
+# tolerance for error in the theta value
 mytol = 1e-3
-numiter = 0
-while (done == False):
-    numiter = numiter + 1
-    print(numiter)
-    mmat = np.zeros((dof, dof))
-    rvec = np.zeros(dof)
-    
-    ## each column of x and t forms a time series observation
-    ## this parallelization is for each time series
-    # for wp in range(allx.shape[1]):
-    #     x = allx[:,wp]
-    #     t = allt[:,wp]
-    #     with Parallel(n_jobs=-1) as parallel:
-    #         results = parallel(delayed(mcmc)(burninpaths, numpaths, g, x, t, numsubintervals, i, h, theta, dof) 
-    #                         for i in range(x.shape[0] - 1))
-    #         for res in results:
-    #             mmat += res[0]
-    #             rvec += res[1]
-    #             print("Acceptance rate during burn-in:", res[2])
-    #             print("Acceptance rate post burn-in:", res[3])
 
-    ## this paralleization is for all time series observations in 1 go
-    with Parallel(n_jobs=-1) as parallel:
-        results = parallel(delayed(mcmc)(burninpaths, numpaths, g, allx, allt, numsubintervals, i, j, h, theta, dof) for (i, j) in zip(range(allx.shape[0] - 1), range(allx.shape[1])))
-        for res in results:
-            mmat += res[0]
-            rvec += res[1]
-            print("Acceptance rate during burn-in:", res[2])
-            print("Acceptance rate post burn-in:", res[3])
+# threshold for number of EM iterations, after which EM returns unsuccessfully
+niter = 100
 
-    newtheta = np.linalg.solve(mmat, rvec)
-    check = np.sum(np.abs(newtheta - theta))
-    if (check < mytol):
-        print("finished!")
-        print(check)
-        print(theta)
-        done = True
-    theta = newtheta
-    print(check)
-    print(theta)
+# call to EM which returns the final error and estimated theta value
+error, theta = nb.em(mytol, niter, burninpaths, numpaths, g, allx, allt, numsubintervals, h, theta, dof)
+print("Error", error)
+print("Theta: ", theta)
--- newbridge/harish/createdata.py
+++ newbridge/harish/createdata.py
@@ -3,23 +3,34 @@ import newbridge as nb
 
 # these parameters define the model
 # coef essentially defines the drift function
-mycoef = np.array([1., 2., -1., -0.5])
+true_theta = np.array([1., 2., -1., -0.5])
+
 # g is the diffusion coefficient
-myg = 1/2
+true_g = 0.5
 
 # these are timestepping parameters
+# number of intermediate Euler Maruyama steps
 numsteps = 25000
+
+# number of time steps saved in the synthetic data
 savesteps = 100
+
+# final time step
 ft = 10.0
+
+# Euler Maruyama time step
 h = ft/numsteps
-ic = np.array([1.0, -1.0])
-it = np.array([0.0, 0.0])
+
+# array of initial conditions for x
+ic = np.array([-1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5])
+
+# array of initial time points for t
+it = np.zeros(ic.shape[0])
 
 # create paths
-xout, tout = nb.createpaths(mycoef, myg, numsteps, savesteps, h, ic, it)
+xout, tout = nb.createpaths(true_theta, true_g, numsteps, savesteps, h, ic, it)
 
 # save to file
 import pickle
 with open('nem.pkl','wb') as f:
     pickle.dump([xout,tout], f)
-
