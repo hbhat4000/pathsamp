4153368003d40f9bbdd2ecb19e0a16ffbf55e55b
Harish S. Bhat <hbhat@ucmerced.edu>
2018-03-16 13:36:45 -0700

    Seeing if we can reproduce Mathematica's curves

--- 3d code/createdata.py
+++ 3d code/createdata.py
@@ -15,29 +15,31 @@ degree of freedom for 3rd order hermite polynomial = 64
 theta = np.zeros((prm.dof, prm.dim))
 gvec = np.array([0.25, 0.25, 0.25])
 
-sigma = 1
-rho = 2
-beta = 3
+sigma = 10
+rho = 28
+beta = 8/3
+nrmlz = 0.63161878
+nrmlz2 = nrmlz**2
 d_param = prm.data(theta, gvec)
-d_param.theta[4, 0] = sigma 	# sigma * y 	in f0
-d_param.theta[1, 0] = -sigma 	# sigma * x 	in f0
-d_param.theta[1, 1] = rho	 	# rho * x 		in f1
-d_param.theta[13, 1] = -1	 	# x * z 		in f1
-d_param.theta[4, 1] = -1 		# y 			in f1
-d_param.theta[10, 2] = 1 		# x * y 		in f2
-d_param.theta[7, 2] = -beta 		# beta * z 		in f2 
+d_param.theta[4, 0] = sigma/nrmlz 	# sigma * y 	in f0
+d_param.theta[1, 0] = -sigma/nrmlz 	# sigma * x 	in f0
+d_param.theta[1, 1] = rho/nrmlz	 	# rho * x 		in f1
+d_param.theta[13, 1] = -1./nrmlz2	# x * z 		in f1
+d_param.theta[4, 1] = -1./nrmlz 	# y 			in f1
+d_param.theta[10, 2] = 1./nrmlz2 	# x * y 		in f2
+d_param.theta[7, 2] = -beta/nrmlz 	# beta * z 		in f2 
 
 # create paths
 """
 The default parameters for Euler-Maruyama are:
 euler_param = prm.euler_maruyama(numsteps = 25000, savesteps = 100, ft = 10., ic, it, numpaths)
 """
-ic = np.array([[1., 1., 1.]])
+ic = 7.8*np.array([[1., 1., 1.]])
 it = np.zeros((ic.shape[0]))
 euler_param = prm.euler_maruyama(ic, it)
 xout, tout = nb.createpaths(d_param, euler_param)
 
 # save to file
 import pickle
-with open('nem_3D.pkl','wb') as f:
+with open('nem_3D_nrmlz.pkl','wb') as f:
     pickle.dump([xout, tout, d_param, euler_param], f)
