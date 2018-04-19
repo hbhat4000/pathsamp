71afdb176f6b3e4335027b9472ac99f8c3a8d7d9
Shagun Rawat <shagunrawat27@gmail.com>
2018-04-18 18:03:08 -0700

    Adding results for varying noise levels from the cluster run

--- 1d code/EM.py
+++ 1d code/EM.py
@@ -4,7 +4,7 @@ import parameters as prm
 
 # load data
 import pickle
-with open('nem_1D_noise1.pkl','rb') as f:
+with open('nem_1D_noise9.pkl','rb') as f:
     allx, allt, x_without_noise, euler_param, sim_param = pickle.load(f)
 
 data_param = prm.data(theta = 0.5 * np.random.rand(prm.dof, prm.dim), gvec = sim_param.gvec)
@@ -27,5 +27,5 @@ print("Theta: ", theta_list)
 
 # save to file
 import pickle
-with open('1D_result_noise1.pkl','wb') as f:
+with open('1D_result_noise9.pkl','wb') as f:
     pickle.dump([error_list, theta_list, em_param, data_param], f)
